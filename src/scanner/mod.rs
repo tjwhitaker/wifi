9d190ffb44169d9f4c487dc1cdf9ee29d362be53
Tim Whitaker <camp4climber@gmail.com>
2017-07-12 11:19:36 -0600

    Make network struct fields public

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -4,21 +4,21 @@ mod windows;
 
 #[derive(Debug)]
 pub struct Network {
-    agr_ctl_rssi: i32,
-    agr_ext_rssi: i32,
-    agr_ctl_noise: i32,
-    agr_ext_noise: i32,
-    state: String,
-    op_mode: String, 
-    last_tx_rate: i32,
-    max_rate: i32,
-    last_assoc_status: i32,
-    wireless_auth: String,
-    link_auth: String,
-    bssid: String,
-    ssid: String,
-    mcs: i32,
-    channel: String,
+    pub agr_ctl_rssi: i32,
+    pub agr_ext_rssi: i32,
+    pub agr_ctl_noise: i32,
+    pub agr_ext_noise: i32,
+    pub state: String,
+    pub op_mode: String, 
+    pub last_tx_rate: i32,
+    pub max_rate: i32,
+    pub last_assoc_status: i32,
+    pub wireless_auth: String,
+    pub link_auth: String,
+    pub bssid: String,
+    pub ssid: String,
+    pub mcs: i32,
+    pub channel: String,
 }
 
 pub fn get_info() -> Network {
